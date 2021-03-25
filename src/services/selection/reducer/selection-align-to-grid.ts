2bd7de53cf677b2ff36d785d4b6c7adc3b806ef9
RoboPhred <robophred@gmail.com>
2021-03-24 17:27:48 -0700

    Snap to grid option for joints

--- src/services/selection/reducer/selection-align-to-grid.ts
+++ src/services/selection/reducer/selection-align-to-grid.ts
@@ -6,9 +6,14 @@ import { pointEquals } from "@/geometry";
 
 import { isSelectionAlignToGridAction } from "@/actions/selection-align-to-grid";
 import { moveElement } from "@/actions/element-move";
+import { moveWireJoint } from "@/actions/wire-joint-move";
 
 import { elementPositionsByElementIdSelector } from "@/services/circuit-layout/selectors/element-positions";
-import { applyGridElementSnapSelector } from "@/services/circuit-editor-drag/selectors/snap";
+import {
+  applyGridElementSnapSelector,
+  applyGridJointSnapSelector,
+} from "@/services/circuit-editor-drag/selectors/snap";
+import { wireJointPositionByJointIdSelector } from "@/services/circuit-graph/selectors/wire-positions";
 
 export default function selectionAlignToGridReducer(
   state: AppState = defaultAppState,
@@ -18,7 +23,7 @@ export default function selectionAlignToGridReducer(
     return state;
   }
 
-  const { selectedElementIds } = state.services.selection;
+  const { selectedElementIds, selectedJointIds } = state.services.selection;
 
   // Align elements.
   const elementPositions = elementPositionsByElementIdSelector(state);
@@ -31,6 +36,20 @@ export default function selectionAlignToGridReducer(
     const snappedPos = applyGridElementSnapSelector(state, elementPos);
     if (!pointEquals(elementPos, snappedPos)) {
       state = rootReducer(state, moveElement(elementId, snappedPos));
+    }
+  }
+
+  // Align joints
+  const jointPositions = wireJointPositionByJointIdSelector(state);
+  for (const jointId of selectedJointIds) {
+    const jointPos = jointPositions[jointId];
+    if (!jointPos) {
+      continue;
+    }
+
+    const snappedPos = applyGridJointSnapSelector(state, jointPos);
+    if (!pointEquals(jointPos, snappedPos)) {
+      state = rootReducer(state, moveWireJoint(jointId, snappedPos));
     }
   }
 
