cba020d26fccbbe25498b5f8b4af1ad0d5ec8147
RoboPhred <robophred@gmail.com>
2021-04-23 18:21:25 -0700

    Work around firefox bug in setInterval

--- src/services/simulator-control/saga/mode-run.ts
+++ src/services/simulator-control/saga/mode-run.ts
@@ -1,5 +1,5 @@
-import { SagaIterator } from "redux-saga";
-import { takeLeading, select, put, delay } from "redux-saga/effects";
+import { eventChannel, SagaIterator, buffers } from "redux-saga";
+import { takeLeading, select, put, take, call } from "redux-saga/effects";
 
 import { ACTION_SIM_START } from "@/actions/sim-start";
 import { ACTION_SIM_PAUSE, pauseSim } from "@/actions/sim-pause";
@@ -17,7 +17,7 @@ export default function* runModeSaga() {
 }
 
 function* handleRunSim(): SagaIterator {
-  while (true) {
+  runSimLoop: while (true) {
     const isRunning = yield select(isSimRunningSelector);
     if (!isRunning) {
       break;
@@ -31,8 +31,56 @@ function* handleRunSim(): SagaIterator {
       return;
     }
 
-    const tps = yield select(ticksPerSecondSelector);
-    const timeToWait = Math.max(Math.ceil(1000 / tps), 1);
-    yield delay(timeToWait);
+    const shouldContinue = yield call(waitNextTick);
+    if (!shouldContinue) {
+      break;
+    }
   }
 }
+
+function* waitNextTick(): SagaIterator<boolean> {
+  const tps = yield select(ticksPerSecondSelector);
+  const timeToWait = Math.max(Math.ceil(1000 / tps), 1);
+  const endWait = performance.now() + timeToWait;
+  while (true) {
+    const timestamp = yield take(animationFrameChannel);
+    const isRunning = yield select(isSimRunningSelector);
+    if (!isRunning) {
+      return false;
+    }
+
+    if (endWait <= timestamp) {
+      return true;
+    }
+  }
+}
+
+// We cannot use redux-saga delay here, as it uses setInterval.
+// On firefox, setInterval has been broken for going on 2 years now:
+// https://bugzilla.mozilla.org/show_bug.cgi?id=1566900
+// Like most bugs submitted to open source projects, it's been marked WontFix.
+// See test here: http://mozilla.pettay.fi/moztests/setInterval16.html
+// TODO: Clean this up.  This fires channel events like mad.
+// Ideally, this would know how long to wait and only emit events when the time is hit.
+const animationFrameChannel = eventChannel((listener) => {
+  let active = true;
+
+  function awaitAnimationFrame() {
+    if (!active) {
+      return;
+    }
+
+    requestAnimationFrame((timestamp) => {
+      if (!active) {
+        return;
+      }
+      listener(timestamp);
+      awaitAnimationFrame();
+    });
+  }
+
+  awaitAnimationFrame();
+  return () => {
+    active = false;
+  };
+}, buffers.sliding(1));
